37690f42780524d84f036fec9a4a48dc82f5543f
IDevKennedyMoreira <kmr.eng.ele@gmail.com>
2024-09-19 20:06:30 -0300

    feat(API-0004): update tests

--- api/veiculos/views.py
+++ api/veiculos/views.py
@@ -1,24 +1,3 @@
-# from rest_framework import viewsets
-# from rest_framework.permissions import IsAuthenticated
-# from .models import Veiculo
-# from .serializers import VeiculoSerializer
-# from rest_framework.response import Response
-
-# class VeiculoViewSet(viewsets.ModelViewSet):
-#     queryset = Veiculo.objects.all()
-#     serializer_class = VeiculoSerializer
-#     permission_classes = [IsAuthenticated]
-
-#     def update(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         status = request.data.get('status')
-#         if status in ['CONNECTADO', 'DESCONECTADO']:
-#             instance.status = status
-#             instance.save()
-#             serializer = self.get_serializer(instance)
-#             return Response(serializer.data)
-#         return Response({'error': 'Status inválido'}, status=400)
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
